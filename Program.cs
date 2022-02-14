cfef149af64207ace39cf9d88a978b96622d5604
Anousone-FS <anousone.litd@gmail.com>
2022-02-14 20:02:47 +0700

    add Retained Storage

---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace broker_console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt",
+                    rollingInterval: RollingInterval.Day,
+                    rollOnFileSizeLimit: true)
+                .CreateLogger();
+            // Create the options for our MQTT Broker
+            MqttServerOptionsBuilder options = new MqttServerOptionsBuilder()
+                                                // log
+                                                .WithConnectionBacklog(100)
+                                                // set endpoint to localhost
+                                                .WithDefaultEndpoint()
+                                                // port used will be 707
+                                                .WithDefaultEndpointPort(707)
+                                                // handler for new connections
+                                                .WithConnectionValidator(OnNewConnection)
+                                                // handler for new messages
+                                                .WithApplicationMessageInterceptor(OnNewMessage)
+                                                .WithStorage(new RetainedMessageHandler());
+
+            IMqttServer mqttServer = new MqttFactory().CreateMqttServer();
+            // mqttServer.StartAsync(options.Bu()).GetAwaiter().GetResult();
+            await mqttServer.StartAsync(options.Build());
+            // keep application running until user press a key
+            Console.WriteLine("Broker Started");
+            Console.ReadLine();
+            await mqttServer.StopAsync();
+            Log.CloseAndFlush();
+        }
+
+        public static void OnNewConnection(MqttConnectionValidatorContext context)
+        {
+            if (context.Username != "Hans")
+            {
+                context.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
+                context.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
+                Log.Information(
+                    "New connection: ClientId = {clientId}, Endpoint = {endpoint}, Username = {username}, Password = {password}, Rawpassword = {rawPassword}",
+                    context.ClientId,
+                    context.Endpoint, context.Username, context.Password, context.RawPassword);
+                return;
+            }
+
+            if (context.Password != "Test")
+            {
+                context.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
+                Log.Information(
+                    "New connection: ClientId = {clientId}, Endpoint = {endpoint}, Username = {username}, Password = {password}, Rawpassword = {rawPassword}",
+                    context.ClientId,
+                    context.Endpoint, context.Username, context.Password, context.RawPassword);
+                return;
+            }
+
+            context.ReasonCode = MqttConnectReasonCode.Success;
+            Log.Information(
+                    "New connection: ClientId = {clientId}, Endpoint = {endpoint}",
+                    context.ClientId,
+                    context.Endpoint);
+        }
+
+        static uint MessageCounter = 0;
+        public static void OnNewMessage(MqttApplicationMessageInterceptorContext context)
+        {
+            var payload = context.ApplicationMessage?.Payload == null ? null : Encoding.UTF8.GetString(context.ApplicationMessage?.Payload);
+
+            MessageCounter++;
+
+            Log.Logger.Information(
+                "MessageId: {MessageCounter} - TimeStamp: {TimeStamp} -- Message: ClientId = {clientId}, Topic = {topic}, Payload = {payload}, QoS = {qos}, Retain-Flag = {retainFlag}",
+                MessageCounter,
+                DateTime.Now,
+                context.ClientId,
+                context.ApplicationMessage?.Topic,
+                payload,
+                context.ApplicationMessage?.QualityOfServiceLevel,
+                context.ApplicationMessage?.Retain);
+        }
+    }
+
+    // The implementation of the storage:
+    // This code uses the JSON library "Newtonsoft.Json".
+    public class RetainedMessageHandler : IMqttServerStorage
+    {
+        // private const string Filename = "C:\\MQTT\\RetainedMessages.json";
+        private const string Filename = "RetainedMessages.json";
+
+        public Task SaveRetainedMessagesAsync(IList<MqttApplicationMessage> messages)
+        {
+            // Console.WriteLine("=> Decode Payload: " + Encoding.UTF8.GetString(messages[0].Payload));
+            var json = JsonConvert.SerializeObject(messages);
+            File.WriteAllText(Filename, json);
+            return Task.FromResult(0);
+        }
+
+        public Task<IList<MqttApplicationMessage>> LoadRetainedMessagesAsync()
+        {
+            IList<MqttApplicationMessage> retainedMessages;
+            if (File.Exists(Filename))
+            {
+                var json = File.ReadAllText(Filename);
+                retainedMessages = JsonConvert.DeserializeObject<List<MqttApplicationMessage>>(json);
+            }
+            else
+            {
+                retainedMessages = new List<MqttApplicationMessage>();
+            }
+
+            return Task.FromResult(retainedMessages);
+        }
+    }
+}
